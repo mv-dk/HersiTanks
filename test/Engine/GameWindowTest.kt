eecb136bb806301e78e8d0b7f5c8d2dbda653dd6
Martin Vestergaard <mvestergaard@gmail.com>
2024-11-02 18:44:35 +0100

    Add test project

---
+++ test/Engine/GameWindowTest.kt
@@ -0,0 +1,27 @@
+package Engine
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class GameWindowTest {
+
+    @Test
+    fun setCurrentGameScene() {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+
+    @Test
+    fun update() {
+        // Arrange
+
+        // Act
+
+        // Assert
+        
+    }
+}
