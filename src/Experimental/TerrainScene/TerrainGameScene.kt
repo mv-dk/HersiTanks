0530c7b406dc210f70e757edbcda85ec3c081e26
Martin Vestergaard <mvestergaard@gmail.com>
2024-11-20 00:03:31 +0100

    Basic earthquake effect

--- src/Experimental/TerrainScene/TerrainGameScene.kt
+++ src/Experimental/TerrainScene/TerrainGameScene.kt
@@ -2,10 +2,7 @@ package Experimental.TerrainScene
 
 import Engine.*
 import gameWindow
-import java.awt.AlphaComposite
-import java.awt.Color
-import java.awt.Graphics2D
-import java.awt.Polygon
+import java.awt.*
 import java.awt.event.KeyEvent
 import java.awt.event.MouseEvent
 import java.awt.image.BufferedImage
@@ -34,6 +31,8 @@ class TerrainGameScene(private val parentScene: IGameScene, color: Color, width:
             rasterTerrain.mode = 1
         } else if (e?.keyCode == KeyEvent.VK_2){
             rasterTerrain.mode = 2
+        } else if (e?.keyCode == KeyEvent.VK_3){
+            rasterTerrain.mode = 3
         }
     }
 
@@ -50,6 +49,7 @@ class RasterTerrain(parent: IGameScene, position: Pos2D) : GameObject2(parent, p
     var rasterImage: BufferedImage = BufferedImage(parent.width, parent.height, BufferedImage.BITMASK)
     var mode: Int = 1
     var crumble: Boolean = false
+    var earthquake: Earthquake? = null
 
     init {
         val g = rasterImage.createGraphics()
@@ -114,6 +114,13 @@ class RasterTerrain(parent: IGameScene, position: Pos2D) : GameObject2(parent, p
                 }
             }
             if (crumbleCounter == 0) crumble = false
+        } else if (earthquake != null){
+            val didUpdateTerrain: Boolean = earthquake?.update(this) == true
+            if (!didUpdateTerrain) {
+                earthquake?.remove()
+                earthquake = null
+                startCrumble()
+            }
         }
     }
 
@@ -121,24 +128,117 @@ class RasterTerrain(parent: IGameScene, position: Pos2D) : GameObject2(parent, p
         println("Mouse was clicked at $x,$y")
         if (mode == 1) {
             pokeHole(x,y);
+            startCrumble()
         } else if (mode == 2){
             startCrumble();
+        } else if (mode == 3){
+            startEarthquake(x,y)
         }
     }
 
-    private fun pokeHole(x: Int, y: Int){
+    fun pokeHole(x: Int, y: Int, size: Int = 60){
         val gg = rasterImage.createGraphics()
         gg.color = Color(0, 0, 0, 0)
-        val size = 60
         gg.composite = AlphaComposite.Clear
         gg.fillOval(x - size / 2, y - size / 2, size, size)
+    }
+
+    fun pokeLine(x1: Int, y1: Int, x2: Int, y2: Int, width: Float){
+        val gg = rasterImage.createGraphics()
+        gg.color = Color(0,0,0,0)
+        gg.composite = AlphaComposite.Clear
+        gg.stroke = BasicStroke(width, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)
+        gg.drawLine(x1, y1, x2, y2)
     }
 
     private fun startCrumble(){
         crumble = true
     }
 
+    private fun startEarthquake(x: Int, y:Int){
+        earthquake = Earthquake(x,y, 5, 100, 1.0)
+    }
+
     override fun draw(g: Graphics2D) {
         g.drawImage(rasterImage, null, 0, 0);
     }
+}
+
+class Earthquake(val x: Int, val y: Int, numCracks: Int, val size: Int, val branchProbability: Double){
+    var cracks = mutableListOf<EarthquakeCrack>()
+
+    init {
+        for (i in 0..numCracks-1){
+            cracks.add(createCrack(x.toDouble(), y.toDouble(), size))
+        }
+    }
+
+    fun createCrack(x: Double, y: Double, growDuration: Int): EarthquakeCrack {
+        return EarthquakeCrack(
+            x,
+            y,
+            Vec2D(random.nextDouble(-5.0, 5.0), random.nextDouble(-5.0, 5.0)),
+            growDuration)
+    }
+
+    fun update(terrain: RasterTerrain): Boolean {
+        var didChangeTerrain = false
+        val newCracks = mutableListOf<EarthquakeCrack>()
+        for (crack in cracks){
+            if (crack.growDuration > 0) {
+                crack.growDuration -= 1
+
+                val holeSize = Math.min(4, crack.growDuration/10)
+                val oldX = crack.x
+                val oldY = crack.y
+
+                if (random.nextDouble() < 0.1 || crack.growDuration % 6 == 0){
+                    crack.x -= crack.direction.y
+                    crack.y += crack.direction.x
+                } else {
+                    crack.x += crack.direction.x
+                    crack.y += crack.direction.y
+                    val futureX = crack.x + crack.direction.x * 10.0
+                    val futureY = crack.y + crack.direction.y * 10.0
+                    if (futureX < 0 || futureX > terrain.rasterImage.width || futureY < 0 || futureY > terrain.rasterImage.height ||
+                        terrain.rasterImage.getRGB(futureX.toInt(), futureY.toInt()) == 0){
+                        crack.growDuration = 0
+                        continue
+                    }
+                }
+                terrain.pokeLine(oldX.toInt(), oldY.toInt(), crack.x.toInt(), crack.y.toInt(), holeSize.toFloat())
+                if (random.nextDouble() < 0.9) {
+                    terrain.pokeLine(
+                        oldX.toInt(),
+                        oldY.toInt(),
+                        crack.x.toInt() - 10 + random.nextInt(20),
+                        crack.y.toInt() - 10 + random.nextInt(20),
+                        2f
+                    )
+                }
+                if (random.nextDouble() < branchProbability/30){
+                    if (random.nextDouble() < 0.1){
+                        newCracks.add(createCrack(crack.x, crack.y, size))
+                    } else {
+                        newCracks.add(createCrack(crack.x, crack.y, (crack.growDuration * 1.0).toInt()))
+                    }
+                }
+
+                crack.direction.x += random.nextDouble(-0.10, 0.10)
+                crack.direction.y += random.nextDouble(-0.10, 0.10)
+                didChangeTerrain = true
+            }
+        }
+        cracks.addAll(newCracks)
+
+        return didChangeTerrain
+    }
+
+    fun remove(){
+        cracks.clear()
+    }
+}
+
+class EarthquakeCrack(var x: Double, var y: Double, val direction: Vec2D, var growDuration: Int) {
+
 }
