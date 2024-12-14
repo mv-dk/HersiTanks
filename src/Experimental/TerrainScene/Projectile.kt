306669be1a06cf7b21f94540b27ac0a2a41230f7
Martin Vestergaard <mvestergaard@gmail.com>
2024-12-14 18:30:17 +0100

    Add projectile that explodes in mid air.

--- src/Experimental/TerrainScene/Projectile.kt
+++ src/Experimental/TerrainScene/Projectile.kt
@@ -9,7 +9,7 @@ import java.awt.BasicStroke
 import java.awt.Color
 import java.awt.Graphics2D
 
-class Projectile(val parent: IGameScene, var position: Pos2D, var velocity: Vec2D) : GameObject2(parent, position) {
+open class Projectile(val parent: IGameScene, var position: Pos2D, var velocity: Vec2D) : GameObject2(parent, position) {
     private val stroke = BasicStroke(2f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)
     var size = 3
     var terrain = (parent as TerrainGameScene).rasterTerrain
@@ -64,5 +64,20 @@ class Projectile(val parent: IGameScene, var position: Pos2D, var velocity: Vec2
         val p0 = position.plus(velocity)
         val p1 = position.plus(velocity.times(-1.0))
         g.drawLine(p0.x.toInt(), p0.y.toInt(), p1.x.toInt(), p1.y.toInt())
+    }
+}
+
+class MidairExplodingProjectile(parent: IGameScene, position: Pos2D, velocity: Vec2D): Projectile(parent, position, velocity) {
+    var ticks = 0
+    val minTicks = 30
+
+    override fun update() {
+        super.update()
+        ticks += 1
+        if (ticks > minTicks) {
+            if (velocity.y >= 0) {
+                explode()
+            }
+        }
     }
 }
