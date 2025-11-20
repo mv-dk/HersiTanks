a08e17dc99747e1fb294bd95d07e0ef289e4d749
Martin Vestergaard <mvestergaard@gmail.com>
2025-11-21 00:50:50 +0100

    Fix font loading in jar

--- src/Game/Helpers/FontHelper.kt
+++ src/Game/Helpers/FontHelper.kt
@@ -1,10 +1,7 @@
 package Game.Helpers
 
-import Game.TerrainScene.TankInfoBar
 import java.awt.Font
 import java.awt.GraphicsEnvironment
-import java.io.File
-import java.net.URI
 
 object FontHelper {
     var balooFont: Font? = null
@@ -12,13 +9,8 @@ object FontHelper {
     init {
         val ge: GraphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment()
         if (balooFont == null) {
-            balooFont = Font.createFont(
-                Font.TRUETYPE_FONT, File(
-                    TankInfoBar::class.java.classLoader.getResource(
-                        "BalooBhaijaan-Regular.ttf"
-                    )?.toURI() ?: URI.create("BalooBhaijaan-Regular.ttf")
-                )
-            )
+            val inStream = FontHelper.javaClass.classLoader.getResourceAsStream("BalooBhaijaan-Regular.ttf")
+            balooFont = Font.createFont(Font.TRUETYPE_FONT, inStream)
             ge.registerFont(balooFont)
         }
     }
